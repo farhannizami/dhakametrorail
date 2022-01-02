784ddc1d3400136c0ddbef2230c5968945e90227
Farhan <farhannizami0612@gmail.com>
2022-01-02 23:28:08 +0600

    added farecalc, departure time-date in findroute, js redirection

--- assets/js/customMade.js
+++ assets/js/customMade.js
@@ -1,7 +1,5 @@
 function toggleShowPass(x, a, b) {
-  //   var x = document.getElementById("pass");
-  //   var a = document.getElementById("hide1");
-  //   var b = document.getElementById("hide2");
+ 
   if (x.type === 'password') {
     x.type = 'text';
     a.style.display = "block";
@@ -34,10 +32,6 @@ function searchRoute() {
   var personind = document.getElementById("person-select");
   var person = personind.options[personind.selectedIndex].text;
 
-  //var ppfare = parseInt(person) * 4;
-
-  //if(time==="") console.log("nnnn")
-  //console.log(date+" "+time);
 
   if (stl === "Choose start location" || dtl === "Choose destination" || person === "Choose number" || time === "" || date === "") {
     alert("Please fill all the forms");
@@ -46,28 +40,88 @@ function searchRoute() {
 
     cn.style.display = "none";
     sch.style.display = "block";
+    alert("Route Found");
 
-    const dist = [-1,0,2.3, 3.7, 5.7, 6.7, 8.2, 9.5, 10.5, 12, 14, 17.8, 18.8, 20.2, 21.2, 22.6, 24.1];
-    //console.log(dtselectind.selectedIndex);
-    //console.log(dtselectind.selectedIndex.value);
-    //console.log(dist[dtselectind.selectedIndex]);
+    const minute = []
 
-    var distance  = Math.abs(dist[stselectind.selectedIndex] - dist[dtselectind.selectedIndex]);
-    var ppfare = Math.max(5,Math.round(distance*2.4));
+    for (let i = 0; i < 60; i += 5) {
+      minute.push(i);
+    }
 
-    var totfare = ppfare* parseInt(person);
+    let hr = time.slice(0, 2);
+    let min = time.slice(3, 5);
+    let minInt = parseInt(min);
+    let hrint = parseInt(hr);
 
-    //alert(stl);
+    let dptime = "";
+
+
+    if (minInt > 50) {
+      hrint = (hrint + 1) % 24;
+      hrstr = hrint.toString();
+      if (hrint < 10) hrstr = '0' + hrstr;
+      hrstr += ":00";
+      dptime = hrstr;
+    }
+    else {
+
+      dptime += hr+":";
+      for (let i = 0; i < minute.length; i++) {
+        if (minute[i] >= minInt) {
+          dptime += minute[i].toString();
+          break;
+        }
+      }
+    }
+
+    const dist = [-1, 0, 2.3, 3.7, 5.7, 6.7, 8.2, 9.5, 10.5, 12, 14, 17.8, 18.8, 20.2, 21.2, 22.6, 24.1];
+
+    var distance = Math.abs(dist[stselectind.selectedIndex] - dist[dtselectind.selectedIndex]);
+    var ppfare = Math.max(5, Math.round(distance * 2.4));
+
+    var totfare = ppfare * parseInt(person);
+
     document.getElementById("train-name").value = "MR-11";
     document.getElementById("stl").value = stl;
     document.getElementById("dtl").value = dtl;
+    document.getElementById("dp-date").value = date;
+    document.getElementById("dp-time").value = dptime;
     document.getElementById("ppfare").value = ppfare;
     document.getElementById("tfare").value = totfare;
   }
 
 }
 
-function confirmMsg()
-{
+function confirmMsg() {
   alert("Ticket Booked Successfully");
+  location.replace("../../index.html");
+}
+
+
+function farecalc() {
+  var cn = document.getElementById("selectdata");
+  var sch = document.getElementById("showprice");
+  var stselectind = document.getElementById("start-select");
+  var dtselectind = document.getElementById("dest-select");
+  var personind = document.getElementById("person-select");
+  var person = personind.options[personind.selectedIndex].text;
+
+  if (stl === "Choose start location" || dtl === "Choose destination" || person === "Choose number") {
+    alert("Please fill all the forms");
+  }
+  else {
+
+    sch.style.display = "block";
+    document.getElementById("footer").classList.remove("fixed-bottom");
+    const dist = [-1, 0, 2.3, 3.7, 5.7, 6.7, 8.2, 9.5, 10.5, 12, 14, 17.8, 18.8, 20.2, 21.2, 22.6, 24.1];
+  
+    var distance = Math.abs(dist[stselectind.selectedIndex] - dist[dtselectind.selectedIndex]);
+    var ppfare = Math.max(5, Math.round(distance * 2.4));
+
+    var totfare = ppfare * parseInt(person);
+    document.getElementById("ppfare").value = ppfare;
+    document.getElementById("tfare").value = totfare;
+
+  }
+
 }
