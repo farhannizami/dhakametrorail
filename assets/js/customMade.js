a4b41a9dcec659bd6c0c6d18a8fe36696ff3db01
Farhan <farhannizami0612@gmail.com>
2022-01-14 22:58:16 +0600

    redesigned findroute

--- assets/js/customMade.js
+++ assets/js/customMade.js
@@ -15,11 +15,6 @@ function toggleShowPass(x, a, b) {
 
 function searchRoute() {
 
-
-  let cn = document.getElementById("selectdata");
-  let sch = document.getElementById("searchtrain");
-
-
   let stselectind = document.getElementById("start-select");
   let stl = stselectind.options[stselectind.selectedIndex].text;
 
@@ -36,13 +31,15 @@ function searchRoute() {
   if (stl === "Choose start location" || dtl === "Choose destination" || person === "Choose number" || time === "" || date === "") {
     alert("Please fill all the forms");
   }
+  else if (stl === dtl) {
+    alert("Start location and destination cannot be same");
+  }
   else {
 
-    cn.style.display = "none";
-    sch.style.display = "block";
     alert("Route Found");
 
-    const minute = []
+
+    const minute = [];
 
     for (let i = 0; i < 60; i += 5) {
       minute.push(i);
@@ -83,19 +80,68 @@ function searchRoute() {
     let ppfare = Math.max(5, Math.round(distance * 2.4));
 
     let totfare = ppfare * parseInt(person);
+    
 
-    //random number between 1 to 14
-    let rnd = Math.floor(Math.random() * 14 + 1);
+    localStorage.setItem("start", stl);
+    localStorage.setItem("end", dtl);
+    localStorage.setItem("date", date);
+    localStorage.setItem("time", dptime);
+    localStorage.setItem("per_fare", ppfare);
+    localStorage.setItem("tot_fare", totfare);
 
-    document.getElementById("train-name").value = "MR-" + rnd.toString();
-    document.getElementById("stl").value = stl;
-    document.getElementById("dtl").value = dtl;
-    document.getElementById("dp-date").value = date;
-    document.getElementById("dp-time").value = dptime;
-    document.getElementById("ppfare").value = ppfare;
-    document.getElementById("tfare").value = totfare;
+    location.href = "trainlist.html";
   }
+}
 
+
+function showtrain() {
+  let stl = localStorage.getItem("start");
+  let dtl = localStorage.getItem("end");
+  let date = localStorage.getItem("date");
+  let time = localStorage.getItem("time");
+  let ppfare = localStorage.getItem("per_fare");
+  let totfare = localStorage.getItem("tot_fare");
+ 
+  document.getElementById("stl").value = stl;
+  document.getElementById("dtl").value = dtl;
+  document.getElementById("ppfare").value = ppfare;
+  document.getElementById("tfare").value = totfare;
+
+  let i;
+  for (i = 0; i < 13; i++) {
+    let rnd = Math.floor(Math.random() * 14 + 1);
+    let copy = document.getElementById("traindetails").cloneNode(true);
+    copy.children[0].children[0].children[0].children[1].value = "MR-" + rnd.toString();
+    copy.children[0].children[0].children[1].children[0].children[0].children[1].value = date;
+    if (i == 0) {
+      copy.children[0].children[0].children[1].children[1].children[0].children[1].value = time;
+    }
+    else {
+      let hr = time.slice(0, 2);
+      let min = time.slice(3, 5);
+      let minInt = parseInt(min);
+      let hrInt = parseInt(hr);
+      let dptime = "";
+      minInt += 5;
+      if (minInt > 59) {
+        minInt -= 60;
+        hrInt++;
+      }
+      if (hrInt > 23) {
+        hrInt -= 24;
+      }
+      hrstr = hrInt.toString();
+      if (hrInt < 10) hrstr = '0' + hrstr;
+      dptime = hrstr + ":";
+      let minstr = minInt.toString();
+      if (minInt < 10) minstr = '0' + minstr;
+      dptime += minstr;
+      copy.children[0].children[0].children[1].children[1].children[0].children[1].value = dptime;
+      time = dptime;
+    }
+    copy.style.display = "block";
+    document.getElementById("listoftrain").append(copy);
+  }
 }
 
 function confirmMsg() {
@@ -117,6 +163,9 @@ function farecalc() {
 
   if (stl === "Choose start location" || dtl === "Choose destination" || person === "Choose number") {
     alert("Please fill all the forms");
+  }
+  else if (stl === dtl) {
+    alert("Start location and destination cannot be same");
   }
   else {
 
@@ -156,17 +205,16 @@ function showComment() {
       dc.style.display = "block";
       count++;
     });
-    if(count==0)
-    {
+    if (count == 0) {
       document.getElementById("no-comment").style.display = "block";
     }
   }, function (error) {
     if (error) {
     } else {
-      
+
     }
   });
-  
+
   let demo_disappear = document.getElementById("feedback-section");
   demo_disappear.children[1].style.display = "none";
 
@@ -185,17 +233,16 @@ function showComment() {
       dc.style.display = "block";
       count++;
     });
-    if(count==0)
-    {
+    if (count == 0) {
       document.getElementById("no-report").style.display = "block";
     }
   }, function (error) {
     if (error) {
     } else {
-      
+
     }
   });
-  
+
   demo_disappear = document.getElementById("missing-section");
   demo_disappear.children[1].style.display = "none";
 
